92b412b1b11dd9379219b3769ffb401c8154f0e9
guzhenfu <guzhenfu@shcem.com>
2016-04-27 11:19:25 +0800

    add textview space

--- app/src/main/java/com/gzfgeh/CustomTextView/MyTextView.java
+++ app/src/main/java/com/gzfgeh/CustomTextView/MyTextView.java
@@ -29,6 +29,7 @@ public class MyTextView extends TextView {
     private int width;
     private Paint paint;
     private int textHeight;
+    private int yPadding;
     int count;
     //记录每个字的二维数组
     int[][] position;
@@ -72,7 +73,8 @@ public class MyTextView extends TextView {
         paint.setTypeface(Typeface.DEFAULT);
         paint.setTextSize(size);
         Paint.FontMetrics fm = paint.getFontMetrics();// 得到系统默认字体属性
-        textHeight = (int) (Math.ceil(fm.descent - fm.top) + 5);// 获得字体高度
+        textHeight = (int) (Math.ceil(fm.descent - fm.ascent - size/4));// 获得字体高度
+        yPadding = Utils.dipToPx(this.getContext(), 10f);
 
         getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -119,77 +121,24 @@ public class MyTextView extends TextView {
         int x = 0;
         char ch;
         position = new int[count][2];
+        float textWith = 0f;
         for (int i=0; i<count; i++){
             ch = content.charAt(i);
             String str = String.valueOf(ch);
             Rect rect = new Rect();
             paint.getTextBounds(str, 0, 1, rect);
             int strwidth = rect.width();
-            float textWith = strwidth;
+            textWith = Math.max(strwidth, textWith);
+        }
+
+        for (int i=0; i<count; i++){
             if (i != 0){
                 x += (width - count * textWith)/(count - 1);
             }
             position[i][0] = x;
             position[i][1] = textHeight;
             x += textWith;
-            this.setHeight(textHeight + 10);
+            this.setHeight(textHeight + yPadding);
         }
     }
-
-
-//    public void getPositions(String content) {
-//        this.content = content;
-//        char ch;
-//        //输入点的 x的坐标
-//        int x = 0;
-//        //当前行数
-//        int lineNum = 1;
-//        count = content.length();
-//        //初始化字体位置数组
-//        position = new int[count][2];
-//        for (int i = 0; i < count; i++) {
-//            ch = content.charAt(i);
-//            String str = String.valueOf(ch);
-//
-//            //根据画笔获得每一个字符的显示的rect 就是包围框（获得字符宽度）
-//            Rect rect = new Rect();
-//            paint.getTextBounds(str, 0, 1, rect);
-//            int strwidth = rect.width();
-//            //对有些标点做些处理
-//            if (str.equals("《") || str.equals("（")) {
-//                strwidth += xPaddingMin * 2;
-//            }
-//            //当前行的宽度
-//            float textWith = strwidth;
-//            //没画字前预判看是否会出界
-//            x += textWith;
-//            //出界就换行
-//            if (x > width) {
-//                lineNum++;// 真实的行数加一
-//                x = 0;
-//            } else {
-//                //回到预判前的位置
-//                x -= textWith;
-//            }
-//            //记录每一个字的位置
-//            position[i][0] = x;
-//            position[i][1] = textHeight * lineNum + yPadding * (lineNum - 1);
-//            //判断是否是数字还是字母 （数字和字母应该紧凑点）
-//            //每次输入完毕 进入下一个输入位置准备就绪
-//            if (isNumOrLetters(str)) {
-//                x += textWith + xPaddingMin;
-//            } else {
-//                x += textWith + xPadding;
-//            }
-//        }
-//        //根据所画的内容设置控件的高度
-//        this.setHeight((textHeight + yPadding) * lineNum);
-//    }
-//
-//    public boolean isNumOrLetters(String str) {
-//        String regEx="^[A-Za-z0-9_]+$";
-//        Pattern p= Pattern.compile(regEx);
-//        Matcher m=p.matcher(str);
-//        return m.matches();
-//    }
 }
