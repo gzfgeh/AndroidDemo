b48889632f70b29f25772ffe0c60729803e864f1
guzhenfu <gzfgeh@gmail.com>
2017-01-12 21:34:59 +0800

    add file

--- ttdemo/src/main/java/com/gzfgeh/ttdemo/PowerOnReceiver.java
+++ ttdemo/src/main/java/com/gzfgeh/ttdemo/PowerOnReceiver.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class PowerOnReceiver extends BroadcastReceiver {
+    private static final String ACTION_BOOT = "android.intent.action.BOOT_COMPLETED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         /*判断是否与action匹配*/
-//        if(action.equals("android.intent.action.BOOT_COMPLETED")) {
+        if(action.equals(ACTION_BOOT)) {
 //            Intent i = new Intent(context, MainActivity.class);
 //            i.putExtra("main_activity", "PowerOnReceiver");
 //            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 //            context.startActivity(i);
-//            Toast.makeText(context, "main_activity", Toast.LENGTH_SHORT).show();
-//        }
+            Toast.makeText(context, "main_activity", Toast.LENGTH_SHORT).show();
+        }
         Toast.makeText(context, "main_activity", Toast.LENGTH_SHORT).show();
     }
 }
