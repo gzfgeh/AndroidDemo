0c84b5aac4650841fc2aedf9b491c5e1fef445f7
guzhenfu <guzhenfu@shcem.com>
2017-01-20 17:52:19 +0800

    add xposed project

--- xposeddemo/src/main/java/com/gzfgeh/xposed/MainActivity.java
+++ xposeddemo/src/main/java/com/gzfgeh/xposed/MainActivity.java
@@ -1,13 +1,99 @@
 package com.gzfgeh.xposed;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationManager locationManager;
+
+    private TextView locationTxt, imeiTxt;
+
+    @SuppressLint("NewApi")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_demo);
+
+        locationTxt = (TextView) findViewById(R.id.location);
+        imeiTxt = (TextView) findViewById(R.id.imei);
+
+        //获取地理位置管理器
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //获取Location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        if(location!=null){
+            //不为空,显示地理位置经纬度
+            showLocation(location);
+        }
+        //监视地理位置变化
+        locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 3000, 1, locationListener);
+
+        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId();
+        imeiTxt.setText("imei:"+imei);
+
     }
+
+    /**
+     * 显示地理位置经度和纬度信息
+     * @param location
+     */
+    private void showLocation(Location location){
+        String locationStr = "纬度：" + location.getLatitude() + ",经度：" + location.getLongitude();
+        locationTxt.setText(locationStr);
+        Log.i("jw", "location:"+locationStr);
+    }
+
+    /**
+     * LocationListern监听器
+     * 参数：地理位置提供器、监听位置变化的时间间隔、位置变化的距离间隔、LocationListener监听器
+     */
+
+    LocationListener locationListener =  new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //如果位置发生变化,重新显示
+            showLocation(location);
+
+        }
+    };
+
 }
